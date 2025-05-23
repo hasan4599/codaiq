e5f2c8e8edb533978d0127b298ceb8f79b187075
hasan4599 <99hasan@live.de>
2025-05-23 19:32:06 +0000

    chore: fix dependencies and update config files

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-<<<<<<< HEAD
-  // Keine experimental.appDir-Flag mehr nötig:
-  // Der /app-Ordner wird ab Next.js 13.4+ automatisch erkannt.
-  // Hier kannst du weitere Konfigurationen hinzufügen, z. B.:
-  //
-  // images: {
-  //   domains: ['example.com'],
-  // },
-  // webpack(config) { … return config; },
+  reactStrictMode: true
 };
 
-=======
-  experimental: {
-    appDir: true
-  }
-};
->>>>>>> 8525796 (chore: add next.config.js to enable appDir)
 module.exports = nextConfig;
