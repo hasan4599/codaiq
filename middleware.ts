5e646405bb40386d35c9ab7b3a9b6bd7092aa859
Lukman Sk <skd1746289@gmail.com>
2025-05-28 16:27:28 +0530

    fix:middleware-edgeruntime

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-export default clerkMiddleware((auth, request) => {
-  try {
-    // Let Clerk handle the authentication
-    return NextResponse.next();
-  } catch (error) {
-    console.error("Clerk middleware error:", error);
-    return NextResponse.next();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!_next).*)", "/"],
+  unstable_allowDynamic: ["**/node_modules/@clerk/**"],
 };
